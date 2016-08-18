4a58acfb7005c1f9850a32a416a2614d8539e8b6
Daiei Kobayashi <kdaiei@hotmail.com>
2016-08-18 13:50:17 +0900

    初期設定済み

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Instagram
 //
 //  Created by kobayashi on 2016/08/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImageSelectViewController.swift
 //  Instagram
 //
 //  Created by kobayashi on 2016/08/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ImageSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
