7ea47958f98d602b6562798df6df59a366f9f40c
Daiei Kobayashi <kdaiei@hotmail.com>
2016-08-19 10:46:42 +0900

    ImageSelectViewController.swiftにメソッド追加

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ImageSelectViewController: UIViewController {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func handleLibraryButton(sender: AnyObject) {
+        // ライブラリ（カメラロール）を指定してピッカーを開く
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            presentViewController(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCameraButton(sender: AnyObject) {
+        // カメラを指定してピッカーを開く
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = UIImagePickerControllerSourceType.Camera
+            presentViewController(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCancelButton(sender: AnyObject) {
+        // 画面を閉じる
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +46,30 @@ class ImageSelectViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // 写真を撮影/選択した時に呼ばれるメソッド
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if info[UIImagePickerControllerOriginalImage] != nil {
+            // 撮影/選択された画像を取得する
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImagePickerControllerDelegate
+            
+            // ここでpresentViewDontrollerを呼び出しても表示されないためメソッドが終了してから呼ばれるようにする
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                // AdobeImageEditorを起動する
+                let adobeViewController = AdobeUXImageEditorViewController(image: image)
+                adobeViewController.delegate = self
+                self.presentViewController(adobeViewController, animated: true, completion: nil)
+            }
+        }
+        
+        // 閉じる
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // 閉じる
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
