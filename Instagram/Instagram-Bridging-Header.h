b9912e3e0872d123ce15f85fef5c6efe669a19b5
Daiei Kobayashi <kdaiei@hotmail.com>
2016-08-23 11:41:52 +0900

    Instagram-Bridging-Header.hがコミットできなかった問題を解決

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by kobayashi on 2016/08/19.
+//  Copyright © 2016年 hirotaka.kobayashi. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+
+#endif /* Instagram_Bridging_Header_h */
